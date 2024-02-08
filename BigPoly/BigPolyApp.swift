d0a3acdae780e96707743ad9a4e284d7345b363c
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-02-08 09:19:21 -0500

    Initial Commit

---
+++ BigPoly/BigPolyApp.swift
@@ -0,0 +1,19 @@
+//   BigPolyApp.swift
+//   BigPoly
+//
+//   Created by: Grant Perry on 2/8/24 at 9:19 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct BigPolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigPoly/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   BigPoly
+//
+//   Created by: Grant Perry on 2/8/24 at 9:19 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
